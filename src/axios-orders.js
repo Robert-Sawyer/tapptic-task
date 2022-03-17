f19698fb2375051f4466e39f37b282fcaa2e12bd
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2022-03-17 18:09:18 +0100

    Improvements

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://form-app-project-58266.firebaseio.com/'
+    baseURL: 'https://someendpoint.free.beeceptor.com/'
 });
 
 export default instance;
